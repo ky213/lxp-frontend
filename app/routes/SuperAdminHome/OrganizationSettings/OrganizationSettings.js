05653585ba6db372bf41809b5de372c699288631
ky <y.keddaoui@gmail.com>
2020-08-27 13:44:31 +0100

    fixes

--- app/routes/SuperAdminHome/OrganizationSettings/OrganizationSettings.js
+++ app/routes/SuperAdminHome/OrganizationSettings/OrganizationSettings.js
@@ -402,7 +402,7 @@ const OrganizationSettings = (props) => {
                                 />
                               </Col>
                             </FormGroup>
-                            {groups.length && (
+                            {groups.length > 0 && (
                               <FormGroup row>
                                 <Label for="group" sm={3}>
                                   Default Group

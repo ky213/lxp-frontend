6d7305149c79c85118261c586df71d5a8b6cef52
Tomasz Wojciechowski <tomasz.wojciechowski@alwasaet.com>
2020-09-20 11:08:50 +0200

    revert

--- app/config/app-config.js
+++ app/config/app-config.js
@@ -1,8 +1,5 @@
-// import config from "environment";
+import config from "environment";
 
-// export const appConfig = {
-//     apiUrl: window && window._env_ && window._env_.API_URL || config.apiUrl
-// }
 export const appConfig = {
-    apiUrl: 'https://api.staging.lxpia.com/api'
+    apiUrl: window && window._env_ && window._env_.API_URL || config.apiUrl
 }

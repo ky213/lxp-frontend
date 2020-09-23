05c5c7838ac16d973a1d9cf99075962880226b33
ky <y.keddaoui@gmail.com>
2020-09-23 19:19:11 +0100

    changed login text

--- app/routes/components/Pages/HeaderAuth.js
+++ app/routes/components/Pages/HeaderAuth.js
@@ -5,32 +5,28 @@ import { Link } from 'react-router-dom';
 import { LogoThemed } from './../LogoThemed/LogoThemed';
 
 const HeaderAuth = (props) => (
-    <div className="mb-1 mt-5">
-        <div className="mb-2 text-left header-auth__logo">
-            <Link to="/" className="d-inline-block">
-                <LogoThemed checkBackground height="25" fill="#F4F4F4" />
-            </Link>
-        </div>
-        <p className="mb-5 text-left header-auth__subtitle">
-            Learning<br />
-            Experience <br/>
-            Platform
-        </p>
-        <h1 className="text-left mb-3 header-auth__title">
-            { props.title }
-        </h1> 
+  <div className="mb-1 mt-5">
+    <div className="mb-2 text-left header-auth__logo">
+      <Link to="/" className="d-inline-block">
+        <LogoThemed checkBackground height="25" fill="#F4F4F4" />
+      </Link>
     </div>
-)
+    <h4 className="mb-5 text-left header-auth__subtitle">
+      Learning Experience Platform
+    </h4>
+    <h1 className="text-left mb-3 header-auth__title">{props.title}</h1>
+  </div>
+);
 HeaderAuth.propTypes = {
-    icon: PropTypes.node,
-    iconClassName: PropTypes.node,
-    title: PropTypes.node,
-    text: PropTypes.node,
+  icon: PropTypes.node,
+  iconClassName: PropTypes.node,
+  title: PropTypes.node,
+  text: PropTypes.node,
 };
 HeaderAuth.defaultProps = {
-    title: "Waiting for Data...",
-    text: "LXP",
-    iconClassName: "text-theme"
+  title: 'Waiting for Data...',
+  text: 'LXP',
+  iconClassName: 'text-theme',
 };
 
 export { HeaderAuth };

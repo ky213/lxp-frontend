a3fe0c1e4676ed8098c781cfa3ffd4e20334bef1
ky <y.keddaoui@gmail.com>
2020-09-26 07:45:48 +0100

    dashboard table expand

--- app/routes/DashboardNew/components/LearnersTable.js
+++ app/routes/DashboardNew/components/LearnersTable.js
@@ -1,15 +1,44 @@
 import React, { useState } from 'react';
-import { Table, Card, CardFooter } from '@/components';
+import {
+  Table,
+  Card,
+  CardFooter,
+  Button,
+  Collapse,
+  CardBody,
+} from '@/components';
 import { Paginations } from '@/routes/components/Paginations';
 import moment from 'moment';
 
+const Expand = ({ user }) => {
+  return (
+    <td colSpan="9">
+      <Collapse isOpen={true}>
+        <Card className="border-0">
+          <CardBody>
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+            labore wes anderson cred nesciunt sapiente ea proident.
+          </CardBody>
+        </Card>
+      </Collapse>
+    </td>
+  );
+};
+
 const LearnersTable = ({ users, onPagination, pageId }) => {
+  const [openUser, setOpenUser] = useState('');
+
+  const toggle = (user) =>
+    openUser ? setOpenUser('') : setOpenUser(user.email);
+
   return (
     <Card className="mb-3">
       {
         <Table hover striped responsive>
           <thead>
             <tr>
+              <th></th>
               <th className="align-middle bt-0 text-center" width="20%">
                 Name
               </th>
@@ -39,22 +68,34 @@ const LearnersTable = ({ users, onPagination, pageId }) => {
           <tbody>
             {users &&
               users.map((user) => (
-                <tr>
-                  <td className="align-middle bt-0">{user.name}</td>
-                  <td className="align-middle bt-0">{user.surname}</td>
-                  <td className="align-middle bt-0">{user.email}</td>
-                  <td className="align-middle bt-0">{user.answers_count}</td>
-                  <td className="align-middle bt-0">
-                    {user.response_fail_count}
-                  </td>
-                  <td className="align-middle bt-0">
-                    {user.response_success_count}
-                  </td>
-                  <td className="align-middle bt-0">{user.scores}</td>
-                  <td className="align-middle bt-0">
-                    {moment(user.start_date).format('DD-MM-YYYY')}
-                  </td>
-                </tr>
+                <>
+                  <tr>
+                    <td className="align-middle bt-0">
+                      <i
+                        className={`fa fa-chevron-${
+                          user.email === openUser ? 'down' : 'right'
+                        } fa-fw`}
+                        onClick={() => toggle(user)}
+                        style={{ marginBottom: '1rem' }}
+                      ></i>
+                    </td>
+                    <td className="align-middle bt-0">{user.name}</td>
+                    <td className="align-middle bt-0">{user.surname}</td>
+                    <td className="align-middle bt-0">{user.email}</td>
+                    <td className="align-middle bt-0">{user.answers_count}</td>
+                    <td className="align-middle bt-0">
+                      {user.response_fail_count}
+                    </td>
+                    <td className="align-middle bt-0">
+                      {user.response_success_count}
+                    </td>
+                    <td className="align-middle bt-0">{user.scores}</td>
+                    <td className="align-middle bt-0">
+                      {moment(user.start_date).format('DD-MM-YYYY')}
+                    </td>
+                  </tr>
+                  <tr>{user.email === openUser && <Expand user={user} />}</tr>
+                </>
               ))}
           </tbody>
         </Table>

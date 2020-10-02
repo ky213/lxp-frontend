09076735dfe3775085c7d67b733f1928bd47d65d
ky <y.keddaoui@gmail.com>
2020-10-02 06:43:13 +0100

    fixed webpack warnings

--- app/index.js
+++ app/index.js
@@ -5,39 +5,39 @@ import { render } from 'react-dom';
 
 import App from './components/App';
 
-import {IntlProvider, FormattedMessage} from 'react-intl';
+import { IntlProvider } from 'react-intl';
 
-import en from "./translations/en";
-import hr from "./translations/hr";
+import en from './translations/en';
+import hr from './translations/hr';
 
 const messages = {
-    en,
-    hr
-  };
+  en,
+  hr,
+};
 
 const locale = 'hr';
 
-const flattenMessages = ((nestedMessages, prefix = '') => {
-    if (nestedMessages === null) {
-      return {}
+const flattenMessages = (nestedMessages, prefix = '') => {
+  if (nestedMessages === null) {
+    return {};
+  }
+  return Object.keys(nestedMessages).reduce((messages, key) => {
+    const value = nestedMessages[key];
+    const prefixedKey = prefix ? `${prefix}.${key}` : key;
+
+    if (typeof value === 'string') {
+      Object.assign(messages, { [prefixedKey]: value });
+    } else {
+      Object.assign(messages, flattenMessages(value, prefixedKey));
     }
-    return Object.keys(nestedMessages).reduce((messages, key) => {
-      const value       = nestedMessages[key]
-      const prefixedKey = prefix ? `${prefix}.${key}` : key
-  
-      if (typeof value === 'string') {
-        Object.assign(messages, { [prefixedKey]: value })
-      } else {
-        Object.assign(messages, flattenMessages(value, prefixedKey))
-      }
-  
-      return messages
-    }, {})
-  });
+
+    return messages;
+  }, {});
+};
 
 render(
-    <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
-        <App />
-    </IntlProvider>,
-    document.querySelector('#root')
+  <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
+    <App />
+  </IntlProvider>,
+  document.querySelector('#root')
 );

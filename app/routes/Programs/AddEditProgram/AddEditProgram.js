da93239aed1d3a81ac3cc4f10ca0e4facdf5e33d
ky <y.keddaoui@gmail.com>
2020-10-15 09:30:55 +0100

    fixes

--- app/routes/Programs/AddEditProgram/AddEditProgram.js
+++ app/routes/Programs/AddEditProgram/AddEditProgram.js
@@ -324,6 +324,26 @@ const AddEditProgram = props => {
                                     border: '1px solid  #80808038',
                                     minHeight: '200px',
                                   }}
+                                  modules={{
+                                    toolbar: [
+                                      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+                                      ['blockquote', 'code-block'],
+
+                                      [{ header: 1 }, { header: 2 }], // custom button values
+                                      [{ list: 'ordered' }, { list: 'bullet' }],
+                                      [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+                                      [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+                                      [{ direction: 'rtl' }], // text direction
+
+                                      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+                                      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+                                      [{ font: [] }],
+                                      [{ align: [] }],
+
+                                      ['clean'], // remove formatting button
+                                    ],
+                                  }}
                                 />
                                 <ErrorMessage
                                   name="body"

a7daab701ff8d8841068112451c73e38d1b07a7a
ky <y.keddaoui@gmail.com>
2020-12-16 16:37:10 +0100

    learners home activity tabs

--- app/routes/Activities/components/ActivityCard.js
+++ app/routes/Activities/components/ActivityCard.js
@@ -1,19 +1,37 @@
 import React from 'react'
 import moment from 'moment'
+import classNames from 'classnames'
 
-import { Card, CardHeader, CardBody, CardFooter } from '@/components'
+import { Row, Col, Card, CardHeader, CardBody, CardFooter } from '@/components'
 import { hot } from 'react-hot-loader'
 
 const ActivityCard = ({ activity }) => {
   return (
     <Card>
       <CardBody>
-        <h4>{activity.name}</h4>
-        <h4>Start: {moment(activity.start).format('L')}</h4>
-        <h4>End: {moment(activity.end).format('L')}</h4>
-        <h4>Status: {activity.status}</h4>
-        <h4>Total points: {activity.totalPoints}</h4>
+        <h2 className="text-primary">{activity.name}</h2>
+        <h6>From: {moment(activity.start).format('L')}</h6>
+        <h6>To: {moment(activity.end).format('L')}</h6>
+        <p className="mt-4">{activity.description || 'no description'}</p>
       </CardBody>
+      <CardFooter>
+        <Row>
+          <Col>
+            <h3
+              className={classNames([
+                'badge',
+                { 'badge-success': activity.status === 'Active' },
+                { 'badge-danger': activity.status !== 'Active' },
+              ])}
+            >
+              {activity.status}
+            </h3>
+          </Col>
+          <Col>
+            <h5 className="text-right"> Points: {activity.totalPoints}</h5>
+          </Col>
+        </Row>
+      </CardFooter>
     </Card>
   )
 }

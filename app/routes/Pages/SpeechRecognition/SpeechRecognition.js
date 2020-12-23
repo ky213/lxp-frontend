b282d79ff75bbdf2e86bf6d14de932311bfb9fdf
ky <y.keddaoui@gmail.com>
2020-12-23 08:32:13 +0100

    record mic

--- app/routes/Pages/SpeechRecognition/SpeechRecognition.js
+++ app/routes/Pages/SpeechRecognition/SpeechRecognition.js
@@ -13,17 +13,41 @@ import {
 } from '@/components'
 import { isNil } from 'lodash'
 
+let mediaRecorder
 const SpeechRecognition = () => {
   const [errorMessage, setErrorMessage] = useState(null)
+  const [recording, setRecording] = useState(false)
+  const [audioSrc, setAudioSrc] = useState('')
 
-  const startRecording = () =>
-    getUserMedia({ video: false, audio: true }, function (err, stream) {
-      if (err) {
-        setErrorMessage(`Error: ${err.message || 'unknown error'}`)
-      } else {
-        setErrorMessage(null)
-      }
-    })
+  const handleRecording = () => {
+    if (!recording) {
+      getUserMedia({ audio: true }, function (err, stream) {
+        const chunks = []
+
+        if (err) {
+          setErrorMessage(`Error: ${err.message || 'unknown error'}`)
+        } else {
+          setRecording(true)
+          setErrorMessage(null)
+          mediaRecorder = new MediaRecorder(stream)
+          mediaRecorder.ondataavailable = e => {
+            chunks.push(e.data)
+          }
+
+          mediaRecorder.onStop = e => {
+            const blob = new Blob(chunks, { type: 'audio/wav; codecs=opus' })
+            const audioURL = URL.createObjectURL(blob)
+            console.log('Stoped:', audioURL)
+            setAudioSrc(audioURL)
+          }
+          mediaRecorder.start()
+        }
+      })
+    } else {
+      setRecording(false)
+      mediaRecorder.stop()
+    }
+  }
 
   return (
     <EmptyLayout className="bg-white">
@@ -49,15 +73,19 @@ const SpeechRecognition = () => {
           </Col>
         </Row>
         <Row>
+          <audio src={audioSrc} controls></audio>
           <Col>
             <Button
-              color="info"
+              color={`${recording ? 'danger' : 'info'}`}
               size="lg"
               block
-              outline
-              onClick={startRecording}
+              outline={!recording}
+              onClick={handleRecording}
             >
-              <i className="fa fa-fw fa-microphone"></i> Start Recording
+              <i
+                className={`fa fa-fw fa-${recording ? 'stop' : 'microphone'}`}
+              ></i>{' '}
+              {recording ? 'Stop' : 'Start'} Recording
             </Button>
           </Col>
         </Row>

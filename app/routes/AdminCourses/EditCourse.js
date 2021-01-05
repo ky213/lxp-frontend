d800c811cd5732446e7dcda54fbe8688ccf78aff
ky <y.keddaoui@gmail.com>
2021-01-05 11:53:02 +0100

    XQ-182

--- app/routes/AdminCourses/EditCourse.js
+++ app/routes/AdminCourses/EditCourse.js
@@ -117,7 +117,7 @@ const EditCourse = ({ course, onCancel, finishInsert }) => {
               courseCode: Yup.string()
                 .required('Course code is required')
                 .matches(
-                  /[a-zA-Z0-9\+\_\-\@\&\=\.\,\(\)\:\ \/\?\|\<\>\"\'\!\%\*\\\#\$\^\;\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u02af\u1d00-\u1d25\u1d62-\u1d65\u1d6b-\u1d77\u1d79-\u1d9a\u1e00-\u1eff\u2090-\u2094\u2184-\u2184\u2488-\u2490\u271d-\u271d\u2c60-\u2c7c\u2c7e-\u2c7f\ua722-\ua76f\ua771-\ua787\ua78b-\ua78c\ua7fb-\ua7ff\ufb00-\ufb06]/g,
+                  /^[a-zA-Z0-9()*_\-!#$%^&*,."\'\][]+$/,
                   'course code should be in latin characters and numbers only'
                 ),
             })}

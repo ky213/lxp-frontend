d1a8f5a123270a4bf325084f2c8161d47ca5c757
ksenia <ksenia.bugrim.dev@gmail.com>
2021-01-12 17:19:16 +0200

    fix

--- src/Assets/Images/library.js
+++ src/Assets/Images/library.js
@@ -17,8 +17,8 @@ export const libraryicon = (
       d="M3.66211 1.43994H17.1621V21.4399H3.66211C2.99907 21.4399 2.36318 21.1765 1.89434 20.7077C1.4255 20.2389 1.16211 19.603 1.16211 18.9399V3.93994C1.16211 3.2769 1.4255 2.64102 1.89434 2.17217C2.36318 1.70333 2.99907 1.43994 3.66211 1.43994V1.43994Z"
       stroke="#A9A9A9"
       strokeWidth="2"
-      strokeWinecap="round"
-      strokeWinejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );

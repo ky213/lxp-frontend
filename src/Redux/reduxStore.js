c13c9434b0e500dab343075042b7edb705a86fa0
ky <y.keddaoui@gmail.com>
2021-01-14 08:18:43 +0100

    fixes

--- src/Redux/reduxStore.js
+++ src/Redux/reduxStore.js
@@ -1,13 +1,15 @@
-import { reducer as formReducer } from 'redux-form'
-import { combineReducers, applyMiddleware, createStore, compose } from 'redux'
-import thunkMiddleWare from 'redux-thunk'
-import multi from 'redux-multi'
-import userReducer from './userReducer'
-import commonReducer from './commonReducer'
-import coursesReducer from './coursesReducer'
-import activitiesReducer from './activitiesReducer'
-import notificationsReducer from './notificationsReducer'
-import programsReducer from './programsReducer'
+import { reducer as formReducer } from 'redux-form';
+import { combineReducers, applyMiddleware, createStore, compose } from 'redux';
+import thunkMiddleWare from 'redux-thunk';
+import multi from 'redux-multi';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import userReducer from './userReducer';
+import commonReducer from './commonReducer';
+import coursesReducer from './coursesReducer';
+import activitiesReducer from './activitiesReducer';
+import notificationsReducer from './notificationsReducer';
+import programsReducer from './programsReducer';
 
 let reducers = combineReducers({
   user: userReducer,
@@ -17,14 +19,13 @@ let reducers = combineReducers({
   notifications: notificationsReducer,
   programs: programsReducer,
   form: formReducer,
-})
+});
 
 let store = createStore(
   reducers,
-  compose(
-    applyMiddleware(thunkMiddleWare, multi),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+  process.env.NODE_ENV === 'development'
+    ? composeWithDevTools(applyMiddleware(thunkMiddleWare, multi))
+    : compose(applyMiddleware(thunkMiddleWare, multi))
+);
 
-export default store
+export default store;

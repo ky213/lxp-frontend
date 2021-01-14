725fbb89a4776b01d4a8081fb3c693f13e763e1a
ky <y.keddaoui@gmail.com>
2021-01-14 11:48:15 +0100

    translation fixes

--- src/Utils/i18n.js
+++ src/Utils/i18n.js
@@ -3,20 +3,24 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-const Languages = ['en', 'ru'];
+const Languages = ['en', 'ar'];
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
     fallbackLng: 'en',
     debug: false,
-    detection:{
-        order: ['queryString', 'cookie'],
-        cache: ['cookie']
+    detection: {
+      order: ['queryString', 'cookie'],
+      cache: ['cookie'],
     },
-    interpolation:{
-        escapeValue: false
+    interpolation: {
+      escapeValue: false,
     },
-    
-    whitelist: Languages
-});
+
+    whitelist: Languages,
+  });
 
 export default i18n;

1fb90a9f4c55133d59e0e6e12c3f8a9be29f391d
ky <y.keddaoui@gmail.com>
2021-01-27 20:32:34 +0100

    lesson card

--- src/Pages/Courses/CourseDetails/Components/Body/Components/LessonCard.jsx
+++ src/Pages/Courses/CourseDetails/Components/Body/Components/LessonCard.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 
+import LessonTiming from 'Assets/Icons/LessonTiming.svg';
+import LessonParts from 'Assets/Icons/LessonParts.svg';
+import LessonLevelBeginner from 'Assets/Icons/LessonLevelBeginner.svg';
 import classes from './styles.module.css';
 
 const LessonCard = () => {
   return (
     <div className={classes.main}>
-      <div>
-        <div>Number</div>
-        <div>01</div>
+      <div className={classes.lessonNumber}>
+        <div>
+          <div className={classes.title}>Lesson</div>
+          <div className={classes.number}>01</div>
+        </div>
       </div>
-      <div>
-        <div>assigned by name</div>
-        <div>lesson title</div>
-        <div>desciprtion</div>
+      <div className={classes.separator}></div>
+      <div className={classes.description}>
+        <div className={classes.assignedBy}>assigned by name</div>
+        <div className={classes.lessonName}>lesson title</div>
+        <div className={classes.bottomText}>desciprtion</div>
       </div>
-      <div>
-        <div>1h 30min</div>
-        <div>10 parts</div>
-        <div>beginner level</div>
+      <div className={classes.lessonSpec}>
+        <div>
+          <img src={LessonTiming} alt="lesson_timing" /> <span>1h 30min</span>
+        </div>
+        <div>
+          <img src={LessonParts} alt="lesson_parts" /> <span>10 parts</span>
+        </div>
+        <div>
+          <img src={LessonLevelBeginner} alt="lesson_level" /> <span>beginner level</span>
+        </div>
       </div>
     </div>
   );

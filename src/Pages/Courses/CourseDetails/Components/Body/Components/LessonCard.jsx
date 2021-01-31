be32b5914ee1c770be39f2617697be9e20b62d02
ky <y.keddaoui@gmail.com>
2021-01-31 10:35:13 +0100

    fixes

--- src/Pages/Courses/CourseDetails/Components/Body/Components/LessonCard.jsx
+++ src/Pages/Courses/CourseDetails/Components/Body/Components/LessonCard.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import LessonTiming from 'Assets/Icons/LessonTiming.svg';
-import LessonParts from 'Assets/Icons/LessonParts.svg';
-import LessonLevelBeginner from 'Assets/Icons/LessonLevelBeginner.svg';
+import { orderNumber } from 'Utils/general';
+// import LessonTiming from 'Assets/Icons/LessonTiming.svg';
+// import LessonParts from 'Assets/Icons/LessonParts.svg';
+// import LessonLevelBeginner from 'Assets/Icons/LessonLevelBeginner.svg';
 import classes from './styles.module.css';
 
 const LessonCard = props => {
   const { lesson } = props;
+
   return (
     <NavLink to={`/lessons/${lesson.lessonId}`}>
       <div className={classes.main}>
         <div className={classes.lessonNumber}>
           <div>
             <div className={classes.title}>Lesson</div>
-            <div className={classes.number}>01</div>
+            <div className={classes.number}>{orderNumber(lesson.lessonOrder)}</div>
           </div>
         </div>
         <div className={classes.separator}></div>
         <div className={classes.description}>
-          <div className={classes.assignedBy}>assigned by name</div>
-          <div className={classes.lessonName}>lesson title</div>
-          <div className={classes.bottomText}>desciprtion</div>
+          {/* <div className={classes.assignedBy}>assigned by name</div> */}
+          <div className={classes.lessonName}>{lesson.lessonName}</div>
+          <div className={classes.bottomText}>{lesson.desciprtion || 'no description'}</div>
         </div>
         <div className={classes.lessonSpec}>
-          <div>
+          {/* <div>
             <img src={LessonTiming} alt="lesson_timing" /> <span>1h 30min</span>
           </div>
           <div>
@@ -32,7 +34,7 @@ const LessonCard = props => {
           </div>
           <div>
             <img src={LessonLevelBeginner} alt="lesson_level" /> <span>beginner level</span>
-          </div>
+          </div> */}
         </div>
       </div>
     </NavLink>
